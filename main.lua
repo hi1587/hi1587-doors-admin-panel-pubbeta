8437f96f9c99075287081a60821c3ccb9fa7cee5
hi1587 <darboy16651@gmail.com>
2025-12-25 13:04:10 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/hi1587/hi1587-doors-admin-panel-beta/releases/download/0.0.11/Main.lua"))
+loadstring(game:HttpGet("https://github.com/hi1587/hi1587-doors-admin-panel-pubbeta/releases/download/0.0.11/Main.lua"))
